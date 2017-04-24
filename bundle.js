03b0ece20bfea6cbecff55e93bd4a21dd766ba31
N4A <1813877406@qq.com>
2017-04-24 11:36:22 +0800

    rm complied file

--- bundle.js
+++ bundle.js
@@ -381,14 +381,14 @@ function scene1(scene) {
         scene.addObject(character);
     });
 
-    geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    cubeMesh = new THREE.Mesh( geometry, material );
-    cube = new GameObject(cubeMesh);
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cubeMesh = new THREE.Mesh( geometry, material );
+    var cube = new GameObject(cubeMesh);
     scene.addObject(cube);
 
     var Rotate = __webpack_require__(4);
-    rotateScript = new Rotate();
+    var rotateScript = new Rotate();
     cube.addComponent(rotateScript);
 }
 
